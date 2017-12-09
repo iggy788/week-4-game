80c763cec5cc7fb5a641aaeb5bd4ae72b361cf84
Mike <mike@Mike-Ignaczak-MacBook-Pro.local>
2017-12-09 16:39:14 -0600

    Added Random Number

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,26 @@
-console.log("we are linked");
+// GLOBAL VARIABLES
+// -----------------------------------------------------------------------------------------------
+
+// FUNCTIONS
+// -----------------------------------------------------------------------------------------------
+$(document).ready(function() {
+
+    var matchNumber = '';
+
+    for (var i = 0; i < 9; i++) {
+        var myRand = Math.floor(Math.random() * 9);
+
+        // Create a new div to add this to the html 9 more times
+        matchNumber += myRand;
+        $('#result').text(matchNumber);
+    }
+
+});
+// DEBUGGING PROCESS
+// -----------------------------------------------------------------------------------------------
+var debug = true;
+
+function debug() {
+    console.log('we are linked');
+    console.log(matchNumber);
+}
