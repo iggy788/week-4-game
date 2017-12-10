4bc3734b97e081c0ed53e9b1893fe63bcaeaf72e
Mike <mike@Mike-Ignaczak-MacBook-Pro.local>
2017-12-10 16:34:16 -0600

    new view

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,13 +6,34 @@
 // .img yellow-crystal
 // -----------------------------------------------------------------------------------------------
 // jQuery detects this state of readiness for you. Code included inside $( document ).ready() will only run once the page Document Object Model (DOM) is ready for JavaScript code to execute.
-$(function() {
+
+$(document).ready(function() {
     // GLOBAL VARIABLES
     // -----------------------------------------------------------------------------------------------
     var debug = true;
     // Created a string which will hold the random number to be matched
-    var matchNumber = '';
+    var redCrystal = Math.floor(Math.random() * 11) + 1;
+    var greenCrystal = Math.floor(Math.random() * 11) + 1;
+    var purpleCrystal = Math.floor(Math.random() * 11) + 1;
+    var yellowCrystal = Math.floor(Math.random() * 11) + 1;
+    var randomNumber = Math.floor(Math.random() * (120 - 19 + 1) + 19);
+    var totalScore = '';
 
+    // This way when the user hits clear, we can guarantee a reset of the app.
+    function startGame() {
+        var redCrystal = Math.floor(Math.random() * 11) + 1;
+        var greenCrystal = Math.floor(Math.random() * 11) + 1;
+        var purpleCrystal = Math.floor(Math.random() * 11) + 1;
+        var yellowCrystal = Math.floor(Math.random() * 11) + 1;
+        var randomNumber = Math.floor(Math.random() * (120 - 19 + 1) + 19);
+        var totalScore = 0;
+
+        // For each iteration, generate a new random number between 1 and 9.
+        $('#result').html(randomNumber);
+        // Take this number and then add it to the rest of the string.
+        randomNumber = totalScore + randomNumber;
+        $('#result').empty();
+    }
     // FUNCTIONS
     // -----------------------------------------------------------------------------------------------
     function debug(inDebugString) {
@@ -21,36 +42,65 @@ $(function() {
 
     // MAIN PROCESS
     // -----------------------------------------------------------------------------------------------
-
-    // Then initiate a loop to generate 9 separate numbers
-    for (var i = 0; i < 9; i++) {
-        // For each iteration, generate a new random number between 1 and 9.
-        var myRand = Math.floor(Math.random() * 9);
-
-        // Take this number and then add it to the rest of the string.
-        matchNumber += myRand;
-    }
-    // Once we have our final lotto number, we'll prepend it to the top of our random-number div.
-    $('#result').text(matchNumber);
+    // For each iteration, generate a new random number between 1 and 9.
+    $('#result').html(randomNumber);
 
     // Add on click event to red crystal
-    $('#red-crystal').on('click', function() {
-        alert('It Works');
+    $('#red-crystal').click(function() {
+        totalScore = totalScore + redCrystal;
+        $('#score').html(totalScore);
+        redCrystal = $(this).val();
+        if (totalScore === randomNumber) {
+            alert('You Win!');
+            win();
+        } else if (totalScore > randomNumber) {
+            alert('You lose Sucka!');
+            lose();
+        }
+        console.log('Your Pick: ' + redCrystal);
     });
 
     // Add on click event to green crystal
-    $('#green-crystal').on('click', function() {
-        alert('It Works');
+    $('#green-crystal').click(function() {
+        totalScore = totalScore + greenCrystal;
+        $('#score').html(totalScore);
+        greenCrystal = $(this).val();
+        if (totalScore === randomNumber) {
+            alert('You Win!');
+            win();
+        } else if (totalScore > randomNumber) {
+            alert('You lose Sucka!');
+            lose();
+        }
+        console.log('Your Pick: ' + greenCrystal);
     });
-
     // Add on click event to purple crystal
-    $('#purple-crystal').on('click', function() {
-        alert('It Works');
+    $('#purple-crystal').click(function() {
+        totalScore = totalScore + purpleCrystal;
+        $('#score').html(totalScore);
+        purpleCrystal = $(this).val();
+        if (totalScore === randomNumber) {
+            alert('You Win!');
+            win();
+        } else if (totalScore > randomNumber) {
+            alert('You lose Sucka!');
+            lose();
+        }
+        console.log('Your Pick: ' + purpleCrystal);
     });
-
     // Add on click event to yellow crystal
-    $('#yellow-crystal').on('click', function() {
-        alert('It Works');
+    $('#yellow-crystal').click(function() {
+        totalScore = totalScore + yellowCrystal;
+        $('#score').html(totalScore);
+        yellowCrystal = $(this).val();
+        if (totalScore === randomNumber) {
+            alert('You Win!');
+            win();
+        } else if (totalScore > randomNumber) {
+            alert('You lose Sucka!');
+            lose();
+        }
+        console.log('Your Pick: ' + yellowCrystal);
     });
     // We get the value associated with the crystal the user picked from here
 
@@ -59,17 +109,20 @@ $(function() {
     // If users went over the random number you let them know.
 
     // Once the user has won or lost create an "on-click" event attached to the "#clear" button id.
-    $('#clear').on('click', function() {
-        // Inside the on-click event...
 
-        // 13. Use the jQuery "empty()" method to clear the contents of the "#display" div.
-        $('#result').empty();
-    });
+    // Inside the on-click event...
+
+    // 13. Use the jQuery "empty()" method to clear the contents of the "#display" div.
+
     // Testing
     // -----------------------------------------------------------------------------------------------
     console.log('Debugger is ON');
-    console.log(matchNumber);
-    console.log(myRand);
+    console.log(redCrystal);
+    console.log(greenCrystal);
+    console.log(purpleCrystal);
+    console.log(yellowCrystal);
+    console.log(totalScore);
+    console.log(randomNumber);
 
     // END OF DOCUMENT READY PROCESS
     // -----------------------------------------------------------------------------------------------
